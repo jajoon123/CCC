c095d18f137f8725b588521e98d80004e5506c26
Hwangjajoon <alphaman0315@gmail.com>
2025-06-10 15:37:58 +0900

    Add files via upload

---
+++ 14th/sin_14/Delegate/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+
+    //엘리게이트 선언 위치
+    delegate int MyDelegate(int a, int b);
+
+    class Calculator
+    {
+        public int Plus(int a, int b) //인스턴스 메소드 
+        {
+            return a + b;
+        }
+
+        public static int Minus(int a, int b) //static 정적인 클래스 메소드
+        {
+            return a - b;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //static 없으니까 여기에 접근할 수 있는 인스턴스 생성
+            Calculator Calc = new Calculator(); //클래스 인스턴스
+            MyDelegate Callback; //델리게이트(대리자) 인스턴스
+
+            Callback = Calc.Plus;
+            Console.WriteLine(Callback(5,3));
+            Callback = new MyDelegate(Calc.Plus); //생성자
+            Console.WriteLine(Callback(10,3));
+
+            Callback = Calculator.Minus;
+            Console.WriteLine(Callback(5, 3));
+            Callback = new MyDelegate(Calculator.Minus); //생성자
+            Console.WriteLine(Callback(10, 3));
+
+        }
+    }
+}
---
+++ 14th/sin_14/UsingCallback/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingCallback
+{
+    delegate int Compare(int a, int b);
+
+    internal class Program //다음주 무명 델리게이터
+    {
+
+        static int AscendCompare(int a, int b)
+        { if (a > b) { return 1; } else if (a == b) { return 0; } else { return -1; } }
+        static int DescendCompare(int a, int b)
+        { if (a < b) { return 1; } else if (a == b) { return 0; } else { return -1; } }
+
+        static void BubbleSort(int[] DataSet, Compare Comparer)
+        {
+            int i = 0; //바깥쪽 루프 인덱스 
+            int j = 0; //안쪽 루프 인덱스
+            int temp = 0; //두 값을 서로 바꿀 때 임시 저장소
+            for (i = 0; i < DataSet.Length - 1; i++) //전체 Pass수
+            { 
+                for (j = 0; j < DataSet.Length - (i + 1); j++) //인정한
+                { 
+                    if (Comparer(DataSet[j], DataSet[j + 1]) > 0)
+                    { 
+                        temp = DataSet[j + 1]; DataSet[j + 1] = DataSet[j]; DataSet[j] = temp; 
+                    } 
+                } 
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+            int[] array1 = { 4, 8, 5, 2, 9 };
+            Console.WriteLine("Sorting ascending...");
+            BubbleSort(array1, new Comparer(AscendCompare));
+
+            for(int j=0; j<array1.Length; i++) 
+            {
+                Console.WriteLine($"array1[i]"); 
+            }
+            Console.WriteLine();
+        }
+    }
+}
