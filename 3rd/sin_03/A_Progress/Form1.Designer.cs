34bf9268fa874dac2ac0f40a219ae6c5cb10d5e2
Hwangjajoon <alphaman0315@gmail.com>
2025-09-16 15:35:36 +0900

    Add files via upload

---
+++ 3rd/sin_03/A_Progress/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace A_Progress
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbStatus = new System.Windows.Forms.ProgressBar();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.btuStop = new System.Windows.Forms.Button();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pbStatus
+            // 
+            this.pbStatus.Location = new System.Drawing.Point(146, 79);
+            this.pbStatus.Name = "pbStatus";
+            this.pbStatus.Size = new System.Drawing.Size(483, 23);
+            this.pbStatus.TabIndex = 0;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(163, 193);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(37, 12);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "상태 :";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(344, 188);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(72, 22);
+            this.btnRun.TabIndex = 2;
+            this.btnRun.Text = "진행";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Timer
+            // 
+            this.Timer.Interval = 1000;
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // btuStop
+            // 
+            this.btuStop.Location = new System.Drawing.Point(435, 188);
+            this.btuStop.Name = "btuStop";
+            this.btuStop.Size = new System.Drawing.Size(75, 23);
+            this.btuStop.TabIndex = 3;
+            this.btuStop.Text = "중지";
+            this.btuStop.UseVisualStyleBackColor = true;
+            this.btuStop.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Location = new System.Drawing.Point(541, 187);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(75, 23);
+            this.btnRestart.TabIndex = 4;
+            this.btnRestart.Text = "재시작";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.btuStop);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.pbStatus);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "상태진행";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar pbStatus;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Timer Timer;
+        private System.Windows.Forms.Button btuStop;
+        private System.Windows.Forms.Button btnRestart;
+    }
+}
+
