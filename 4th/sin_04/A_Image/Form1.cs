9c790203be449e886d4ea91fddc86bdc5f4a83df
Hwangjajoon <alphaman0315@gmail.com>
2025-09-16 15:36:23 +0900

    Add files via upload

---
+++ 4th/sin_04/A_Image/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A_Image
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int ImgCount = 0; //이미지 번호
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.picImg.Image = this.ImgList.Images[0];
+            ImgCount = this.ImgList.Images.Count;
+        }
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+
+            ImgCount--;
+            if (ImgCount < 0)
+            {
+             ImgCount = this.ImgList.Images.Count - 1;   
+            }
+
+            this.picImg.Image = this.ImgList.Images[ImgCount];
+        }
+    }
+}
